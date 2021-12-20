2ba31c5898fb98be56a3d5fdb051617454b59936
Wei Wang <46344542+kazuyahooo@users.noreply.github.com>
2021-12-21 00:34:30 +0800

    I'm not really sure
    upload file test

--- form.py
+++ form.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, render_template, redirect, url_for
+from flask_uploads import UploadSet, TEXT, DOCUMENTS, configure_uploads
 import json
 import re
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'development'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
+app.config['UPLOADED_DEF_DEST'] = r'C:\Users\Kazuyahoo\Documents\GitHub\esg_visualize\static\input'
+app.config['UPLOADED_DEF_URL'] = '\\static\\input\\'
+
+usr_doc = UploadSet(name='def', extensions= TEXT + DOCUMENTS)
+configure_uploads(app, usr_doc)
 
 @app.route('/form')
 def formPage():
@@ -12,18 +20,22 @@ def formPage():
 
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
+    if request.method == 'POST' and 'in_abc' in request.files:
+        filename = usr_doc.save(request.files['in_usr_doc'])
+        print(filename)
+        file_url = usr_doc.url(filename)
+        print(file_url)
+        return "Hello world!"
     if request.method == 'POST':
         text = request.form['text']
         text = re.sub(u"\\<.*?\\>", "", text)
-        print(text)
         text = json.dumps(text.split(' '))
-        print(text)
-        #print("post : user => ", text)
         return redirect(url_for('present', text=text, action="post"))
-    else:
-        text = request.args.get('text')
-        #print("get : user => ", user)
-        return redirect(url_for('present', text=text, action="get"))
+
+    # else:
+    #     text = request.args.get('text')
+    #     #print("get : user => ", user)
+    #     return redirect(url_for('present', text=text, action="get"))
 
 
 @app.route('/present/<action>/<text>')
