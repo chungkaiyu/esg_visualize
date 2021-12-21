b57a499418271fe20179f983ca2035bfbd13fc92
Wei Wang <46344542+kazuyahooo@users.noreply.github.com>
2021-12-21 22:16:43 +0800

    Upload Function Added

--- form.py
+++ form.py
@@ -1,46 +1,41 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_uploads import UploadSet, TEXT, DOCUMENTS, configure_uploads
-import json
-import re
+from werkzeug.utils import secure_filename
+import os, re, json
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'development'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
-app.config['UPLOADED_DEF_DEST'] = r'C:\Users\Kazuyahoo\Documents\GitHub\esg_visualize\static\input'
-app.config['UPLOADED_DEF_URL'] = '\\static\\input\\'
+app.config['UPLOADED_DEF_DEST'] = os.getcwd() + '\\static\\input\\'
 
-usr_doc = UploadSet(name='def', extensions= TEXT + DOCUMENTS)
+usr_doc = UploadSet(name='def', extensions= TEXT + DOCUMENTS + tuple(['pdf']))
 configure_uploads(app, usr_doc)
+
+@app.errorhandler(413)
+def request_entity_too_large(error):
+    return '<script>alert("File to large!");window.location.href ="./form";</script>', 413
 
 @app.route('/form')
 def formPage():
     return render_template('form.html')
 
-
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
-    if request.method == 'POST' and 'in_usr_doc' in request.files:
-        try:
-            filename = usr_doc.save(request.files['in_usr_doc'])
-            print(filename)
-            file_url = usr_doc.url(filename)
-            print(file_url)
-            return "Hello world!"
-        except:
-           # return "Whyyyyyyyyyyyyyyy"
-           return '<script>alert("TEST");window.location.href ="./form";</script>'
     if request.method == 'POST':
+        if 'in_usr_doc' in request.files:
+            try:
+                filename = usr_doc.save(request.files['in_usr_doc'])
+                print(filename)
+                file_url = usr_doc.url(filename)
+                print(file_url)
+                return "Oh no, we didn't finish this route."
+            except:
+                return '<script>alert("We only accept the file type with document or .txt");window.location.href ="./form";</script>'
         text = request.form['text']
         text = re.sub(u"\\<.*?\\>", "", text)
         text = json.dumps(text.split(' '))
         return redirect(url_for('present', text=text, action="post"))
-
-    # else:
-    #     text = request.args.get('text')
-    #     #print("get : user => ", user)
-    #     return redirect(url_for('present', text=text, action="get"))
-
+    return '<script>alert("We didn\'t design Get request.");window.location.href ="./form";</script>'
 
 @app.route('/present/<action>/<text>')
 def present(text, action):
@@ -48,6 +43,5 @@ def present(text, action):
     data = file.read()
     return render_template('present.html', text=json.loads(text), action=action, data=json.loads(data))
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
