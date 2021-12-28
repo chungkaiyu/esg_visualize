9da4a05bf8d9ce8dc97fe0fe38cc72f1202933ac
chungkaiyu <chungkaiyu12936@gmail.com>
2021-12-29 01:42:37 +0800

    WTForm
    Fix Tinymce

--- form.py
+++ form.py
@@ -1,18 +1,44 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import (StringField, BooleanField, DateTimeField,
+                     RadioField, SelectField,
+                     TextAreaField, SubmitField)
+from wtforms.validators import DataRequired
+
+
+from flask import Flask, request, session, render_template, redirect, url_for
 from flask_uploads import UploadSet, TEXT, DOCUMENTS, configure_uploads
-from pathlib import Path
+from werkzeug.utils import secure_filename
 import os
 import re
 import json
 
-download_path = (Path.cwd() / 'static/input')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'development'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
-app.config['UPLOADED_DEF_DEST'] = download_path
+app.config['UPLOADED_DEF_DEST'] = os.getcwd() + '\\static\\input\\'
 
 usr_doc = UploadSet(name='def', extensions=TEXT + DOCUMENTS + tuple(['pdf']))
 configure_uploads(app, usr_doc)
+
+
+class CustomForm(FlaskForm):
+    text = StringField('Please enter your text!', validators=[
+        DataRequired()])
+    analytics = RadioField('Analysis Kernel Option', choices=[(
+        'wordLv', 'Word-level'), ('sentLv', 'Sentence-level')])
+    annualReport = SelectField('Annual report', choices=[('Material2020', 'Applied Material 2020'), (
+        'Material2019', 'Applied Material 2019'), ('Material2018', 'Applied Material 2018')])
+    submit = SubmitField("Submit")
+
+
+class MyForm(FlaskForm):
+    name = StringField('你的名字', validators=[DataRequired()])
+    agreed = BooleanField('同意加入這個組織？')
+    gender = RadioField('請輸入性別', choices=[('M', '男生'), ('F', '女生')])
+    hobby = SelectField(
+        '你的興趣', choices=[('sports', '運動'), ('travel', '旅遊'), ('movie', '電影')])
+    others = TextAreaField()
+    submit = SubmitField("確認")
 
 
 @app.errorhandler(413)
@@ -20,9 +46,32 @@ def request_entity_too_large(error):
     return '<script>alert("File to large!");window.location.href ="./form";</script>', 413
 
 
-@app.route('/form')
+@app.route('/form', methods=['GET', 'POST'])
 def formPage():
-    return render_template('form.html')
+    print('Entering')
+    # form = MyForm()
+    # if form.validate_on_submit():
+    #         session['name'] = form.name.data
+    #         session['agreed'] = form.agreed.data
+    #         session['gender'] = form.gender.data
+    #         session['hobby'] = form.hobby.data
+    #         session['others'] = form.others.data
+    #         print('In')
+    #         return redirect(url_for('test'))
+    form = CustomForm()
+    if form.validate_on_submit():
+        session['text'] = form.text.data
+        session['analytics'] = form.analytics.data
+        session['annualReport'] = form.annualReport.data
+        print('In')
+        return redirect(url_for('test'))
+    return render_template('form.html', form=form)
+
+
+@app.route('/test')
+def test():
+    print('Hello')
+    return render_template('test.html')
 
 
 @app.route('/submit', methods=['POST', 'GET'])
@@ -34,7 +83,8 @@ def submit():
                 print(filename)
                 file_url = usr_doc.url(filename)
                 print(file_url)
-                return "Oh no, we didn't finish this route."
+                # return "Oh no, we didn't finish this route."
+                return '<script>alert("Upload done!");window.location.href ="./form";</script>'
             except:
                 return '<script>alert("We only accept the file type with document or .txt");window.location.href ="./form";</script>'
         text = request.form['text']
@@ -52,4 +102,4 @@ def present(text, action):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run(debug=True)
