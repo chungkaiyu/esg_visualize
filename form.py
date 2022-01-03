d9add6aa5fafdd7ef801362b6a6f7c9410f250ee
Wei Wang <46344542+kazuyahooo@users.noreply.github.com>
2022-01-03 23:16:33 +0800

    add Fullpdf in result

--- form.py
+++ form.py
@@ -14,7 +14,6 @@ import time
 import bubble_plot
 
 download_path = (Path.cwd() / 'static/input')
-print(download_path)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'development'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
@@ -76,43 +75,60 @@ def test():
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
     if request.method == 'POST':
-        if 'in_usr_doc' in request.files:
-            try:
-                filename = usr_doc.save(request.files['in_usr_doc'])
-                print(filename)
-                file_url = usr_doc.url(filename)
-                print(file_url)
-                # return "Oh no, we didn't finish this route."
-                # return '<script>alert("Upload done!");window.location.href ="./form";</script>'
-                redirect(url_for('alert', success='True'))
-                time.sleep(5)
-                return '<script>window.location.href ="./form";</script>'
-            except:
-                # return '<script>alert("We only accept the file type with document or .txt");window.location.href ="./form";</script>'
-                time.sleep(5)
-                redirect(url_for('alert', success='False'))
-                return '<script>window.location.href ="./form";</script>'
-        text = request.form['text']
-        text = re.sub(u"\\<.*?\\>", "", text)
-        text = json.dumps(text.split(' '))
-        return redirect(url_for('present', text=text, action="post"))
+        # if 'in_usr_doc' in request.files:
+        #     try:
+        #         filename = usr_doc.save(request.files['in_usr_doc'])
+        #         print(filename)
+        #         file_url = usr_doc.url(filename)
+        #         print(file_url)
+        #         # return "Oh no, we didn't finish this route."
+        #         # return '<script>alert("Upload done!");window.location.href ="./form";</script>'
+        #         redirect(url_for('alert', success='True'))
+        #         time.sleep(5)
+        #         return '<script>window.location.href ="./form";</script>'
+        #     except:
+        #         # return '<script>alert("We only accept the file type with document or .txt");window.location.href ="./form";</script>'
+        #         time.sleep(5)
+        #         redirect(url_for('alert', success='False'))
+        #         return '<script>window.location.href ="./form";</script>'
+        # text = request.form['text']
+        # text = re.sub(u"\\<.*?\\>", "", text)
+        # text = json.dumps(text.split(' '))
+        session['kernel_option'] = request.form['flexRadioDefault']
+        session['report_selector'] = request.form['report-selector']
+        dp = DocProcessor()
+        file_path = os.getcwd() + '/static/input'
+        text = dp.get_file_text(str(file_path + '/Test9-2019-IntelCSR-Report.pdf'))
+        #session['text'] = text
+        return redirect(url_for('present2'))
     return '<script>alert("We didn\'t design Get request.");window.location.href ="./form";</script>'
 
+@app.route('/present2')
+def present2():
+    
+    report = session['report_selector'].split('.')[0]
+    report_path = './static/tmp/' + report + '.txt'
+    text = open(report_path, 'r', encoding='utf-8').read().split()
+    report_kp_path = './static/tmp/' + report + '_key_phrase.txt'
+    data = open(report_kp_path, 'r', encoding='utf-8').read()
+    #file = open('./key_phrase.txt', 'r', encoding='utf-8')
+    #data = file.read()
+    return render_template('present.html', text = text, radio = session['kernel_option'], data=json.loads(data))
 
 @app.route('/present/<action>/<text>')
 def present(text, action):
-    file_path = os.getcwd() + '/static/input'
-    #print(file_path)
-    dp = DocProcessor()
-    text = dp.get_file_text(str(file_path + '/Test9-2019-IntelCSR-Report.pdf'))
     file = open('./key_phrase.txt', 'r', encoding='utf-8')
     data = file.read()
-    return render_template('present.html', text=text, action=action, data=json.loads(data))
+    return render_template('present.html', text=text.split(' '), action=action, data=json.loads(data))
 
 @app.route('/showReports')
 def showReports():
-    path = './static/input/'
-    files = os.listdir(path)
+    path = './static/tmp/'
+    fullname = os.listdir(path)
+    files = list()
+    for i in fullname:
+        if i[:-4] not in files and 'key_phrase' not in i:
+            files.append(i[:-4])
     res = {'files':files}
     return res
 
